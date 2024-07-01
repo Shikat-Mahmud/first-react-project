acd69b92159f346349d10b990480c25df1d1ec26
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-07-02 00:23:52 +0600

    a card created with react

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
 export const App = () => {
-  return <h1>Hello, This is Eftakhar Mahmud.</h1>;
+  return (
+  <div>
+    <div>
+      <img src="img/my-img.jpg" alt="Photo" width="250px" height="auto" />
+    </div>
+    <h1>Movie Title</h1>
+    <h2>Rating: 8.6</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt amet in, harum dolor necessitatibus tenetur reiciendis itaque placeat similique adipisci eaque nemo cum maxime ullam iste ipsam dicta dolores! Ea!</p>
+  </div>
+  );
 };
