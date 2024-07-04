cd832df01914fb27faea7a864a4b82ee2719e2b9
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-07-04 22:47:55 +0600

    dynamically data fetching with looping in jsx

--- src/components/NexflixSeries.jsx
+++ src/components/NexflixSeries.jsx
@@ -1,32 +1,38 @@
 import seriesData from '../api/series.json'
 
 const NetflixSeries = () => {
-    // const movie_name = "Unlocked";
-    // const movie_rating = 9.2;
-    // const summary = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt amet in, harum dolor necessitatibus tenetur reiciendis itaque placeat similique adipisci eaque nemo cum maxime ullam iste ipsam dicta dolores! Ea!";
-    // let age = 13;
-  
-    // let canWatch = "Not Available";
-    // if(age >= 18) {
-    //   canWatch = "Watch Now";
-    // }
-  
-    return (
-      <>
-      <div>
-        <img src={seriesData[0].img_url} alt="Photo" width="250px" height="auto" />
-      </div>
-      <h1>Title: {seriesData[0].name}</h1>
-      <h2>Rating: {seriesData[0].rating}</h2>
-      <p>Summary: {seriesData[0].description}</p>
-      <p>Genre: {seriesData[0].genre}</p>
-      <p>Cast: {seriesData[0].cast}</p>
-      <a href={seriesData[0].watch_url} target='_blank'>
-        <button>Watch Now</button>
-      </a>
-    </>
-    );
-  };
+  // const movie_name = "Unlocked";
+  // const movie_rating = 9.2;
+  // const summary = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt amet in, harum dolor necessitatibus tenetur reiciendis itaque placeat similique adipisci eaque nemo cum maxime ullam iste ipsam dicta dolores! Ea!";
+  // let age = 13;
+
+  // let canWatch = "Not Available";
+  // if(age >= 18) {
+  //   canWatch = "Watch Now";
+  // }
+
+  return (
+    <ul>
+      {seriesData.map((currElem) => {
+        return (
+        <li key={currElem.id}>
+          <div>
+            <img src={currElem.img_url} alt={currElem.name} width="250px" height="auto" />
+          </div>
+          <h1>Title: {currElem.name}</h1>
+          <h2>Rating: {currElem.rating}</h2>
+          <p>Summary: {currElem.description}</p>
+          <p>Genre: {currElem.genre}</p>
+          <p>Cast: {currElem.cast}</p>
+          <a href={currElem.watch_url} target='_blank'>
+            <button>Watch Now</button>
+          </a>
+        </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 
-  export default NetflixSeries;
+export default NetflixSeries;
