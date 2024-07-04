c0de53cce83927511413e2286f7d2e1ee54ded77
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-07-05 00:56:15 +0600

    Series property name change

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -2,14 +2,14 @@ export const SeriesCard = (props) => {
     return (
         <li>
           <div>
-            <img src={props.currElem.img_url} alt={props.currElem.name} width="250px" height="auto" />
+            <img src={props.data.img_url} alt={props.data.name} width="250px" height="auto" />
           </div>
-          <h1>Title: {props.currElem.name}</h1>
-          <h2>Rating: {props.currElem.rating}</h2>
-          <p>Summary: {props.currElem.description}</p>
-          <p>Genre: {props.currElem.genre}</p>
-          <p>Cast: {props.currElem.cast}</p>
-          <a href={props.currElem.watch_url} target='_blank'>
+          <h1>Title: {props.data.name}</h1>
+          <h2>Rating: {props.data.rating}</h2>
+          <p>Summary: {props.data.description}</p>
+          <p>Genre: {props.data.genre}</p>
+          <p>Cast: {props.data.cast}</p>
+          <a href={props.data.watch_url} target='_blank'>
             <button>Watch Now</button>
           </a>
         </li>
