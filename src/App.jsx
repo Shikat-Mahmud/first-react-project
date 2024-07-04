a4e0013c88dc62593fd16d88b9031d15213b3da6
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-07-04 13:10:03 +0600

    component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 export const App = () => {
   return (
   <div>
+    <NetflixSeries/>
+    <NetflixSeries/>
+    <NetflixSeries/>
+    <NetflixSeries/>
+  </div>
+  );
+};
+
+const NetflixSeries = () => {
+  return (
+    <div>
     <div>
       <img src="img/my-img.jpg" alt="Photo" width="250px" height="auto" />
     </div>
-    <h1>Movie Title</h1>
-    <h2>Rating: 8.6</h2>
+    <h1>Movie Title 2</h1>
+    <h2>Rating: 9.5</h2>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt amet in, harum dolor necessitatibus tenetur reiciendis itaque placeat similique adipisci eaque nemo cum maxime ullam iste ipsam dicta dolores! Ea!</p>
   </div>
   );
