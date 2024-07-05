8685d891053179ac07440998c16fbca2ff585f6d
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-07-06 01:49:18 +0600

    custom css added as assignment

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,13 +1,15 @@
+import "../css/Netflix.css"
+
 export const SeriesCard = (props) => {
     const {data} = props; // Destructuring Props
     // const { name, rating, description, genre, cast, watch_url, img_url} = props.data; // Deep Destructuring Props
 
     return (
-        <li>
+        <li className="card-box">
           <div>
-            <img src={data.img_url} alt={props.data.name} width="250px" height="auto" />
+            <img className="card-box-img" src={data.img_url} alt={props.data.name}/>
           </div>
-          <h1>Title: {data.name}</h1>
+          <h1>Name: {data.name}</h1>
           <h2>Rating: {data.rating}</h2>
           <p>Summary: {data.description}</p>
           <p>Genre: {data.genre}</p>
