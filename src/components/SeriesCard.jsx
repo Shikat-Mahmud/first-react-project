5591ffa944b53463bfac3a1604e346a113e420e7
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-07-06 12:57:50 +0600

    used inline css with object

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,22 +1,36 @@
 import "../css/Netflix.css"
 
-export const SeriesCard = (props) => {
-    const {data} = props; // Destructuring Props
-    // const { name, rating, description, genre, cast, watch_url, img_url} = props.data; // Deep Destructuring Props
+// inline css with object 
+const btn_style = {
+  padding: "10px 15px",
+  backgroundColor: "darkblue",
+  color: "aliceblue",
+  cursor: "pointer",
+  border: "none",
+  marginTop: "10px",
+  marginBottom: "20px",
+  borderRadius: "6px",
+}
 
-    return (
-        <li className="card-box">
-          <div>
-            <img className="card-box-img" src={data.img_url} alt={props.data.name}/>
-          </div>
-          <h1>Name: {data.name}</h1>
-          <h2>Rating: {data.rating}</h2>
-          <p>Summary: {data.description}</p>
-          <p>Genre: {data.genre}</p>
-          <p>Cast: {data.cast}</p>
-          <a href={data.watch_url} target='_blank'>
-            <button>Watch Now</button>
-          </a>
-        </li>
-    );
+export const SeriesCard = (props) => {
+  const { data } = props; // Destructuring Props
+  // const { name, rating, description, genre, cast, watch_url, img_url} = props.data; // Deep Destructuring Props
+
+  return (
+    <li className="card-box">
+      <div>
+        <img className="card-box-img" src={data.img_url} alt={props.data.name} />
+      </div>
+      <div className="card-content">
+        <h2>Name: {data.name}</h2>
+        <h4>Rating: {data.rating}</h4>
+        <p>Summary: {data.description}</p>
+        <p>Genre: {data.genre}</p>
+        <p>Cast: {data.cast}</p>
+        <a href={data.watch_url} target='_blank'>
+          <button style={btn_style}>Watch Now</button>
+        </a>
+      </div>
+    </li>
+  );
 };
