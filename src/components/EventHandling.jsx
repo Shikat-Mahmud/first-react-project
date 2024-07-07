c55c29a46e18333fca9b11c30d8a3c0b7d8ad163
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-07-07 13:14:42 +0600

    inline far arrow function used for handleWelcomeUser component

--- src/components/EventHandling.jsx
+++ src/components/EventHandling.jsx
@@ -1,17 +1,38 @@
 export const EventHandling = () => {
     
+    const btn_style = {
+        padding: "10px 15px",
+        backgroundColor: "darkblue",
+        color: "aliceblue",
+        cursor: "pointer",
+        border: "none",
+        marginTop: "10px",
+        marginBottom: "20px",
+        borderRadius: "6px",
+        fontSize: "16px"
+      }
+
     // function handleButtonClick() {
     //     alert("Hey i am onClick");
     // }
 
     // instead of function we can use component in react 
-    const handleButtonClick = () => {
+    const handleButtonClick = (event) => {
+        console.log(event);
+        console.log(event.target);
+        console.log(event.type);
         alert("Hey i am onClick");
+    }
+
+    const handleWelcomeUser = (user) => {
+        console.log(`hey ${user}, welcome`);
     }
 
     return (
         <>
-            <button onClick={handleButtonClick} >Click Me</button>
+            <button style={btn_style} onClick={handleButtonClick} >Click Me</button>
+            <br />
+            <button style={btn_style} onClick={() => handleWelcomeUser("Saikat")} >Click Me</button>
         </>
     );
 }
