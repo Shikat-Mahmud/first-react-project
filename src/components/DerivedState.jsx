84126b1d09ad437ee6d3a36d2cde037f5b87a686
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-07-10 00:29:20 +0600

    useState Hook in the array data

--- src/components/DerivedState.jsx
+++ src/components/DerivedState.jsx
@@ -1,19 +1,33 @@
-const users = [
-    {name: "Saikat", age: 21},
-    {name: "Mahmud", age: 24},
-    {name: "Eftakhar", age: 26},
-    {name: "Abir", age: 19},
-];
+import { useState } from "react";
+
+// const users = [
+//     { name: "Saikat", age: 21 },
+//     { name: "Mahmud", age: 24 },
+//     { name: "Eftakhar", age: 26 },
+//     { name: "Abir", age: 19 }
+// ];
 
 export const DerivedState = () => {
+
+    const [users, setUsers] = useState([
+        { name: "Saikat", age: 21 },
+        { name: "Mahmud", age: 24 },
+        { name: "Eftakhar", age: 26 },
+        { name: "Abir", age: 19 }
+    ]);
+
     return (
         <>
             <div className="main-div">
                 <h1>User List</h1>
                 <ul>
-                    {users.map((currElem) => {
-                        <p><span>{currElem.name}</span> - <span>{currElem.age}</span> years old.</p>
-                    })};
+                    {users.map((currElem, index) => {
+                        return (
+                            <li key={index}>
+                                <p>{index + 1}. {currElem.name} - {currElem.age} years old.</p>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
         </>
